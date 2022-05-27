fa0a65a6f6fea7b296fe6b0386e4ac72b3c8b8c3
Akshaya Shankar <akshaya_shankar@comcast.com>
2022-05-27 21:33:03 +0530

    removed folders

--- ms_scowin_vac_drive/vac_drive/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import VaccineDrive, Student, StudentVaccination
-
-admin.site.register(VaccineDrive)
-admin.site.register(Student)
-admin.site.register(StudentVaccination)
