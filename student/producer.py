9b366e34dea689ce04e8e07c8518f98765a0ccd7
caput-draconis <2021cfse014@wilp.bits-pilani.ac.in>
2022-07-22 23:48:35 +0530

    Dockerised application

--- student/producer.py
+++ student/producer.py
@@ -1,10 +1,18 @@
 import json
+from unittest.mock import DEFAULT
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
-channel = connection.channel()
+# import os
 
 def publish(method, body):
+    # amqp_url = os.environ['AMQP_URL']
+    amqp_url = 'amqp://rabbit_mq?connection_attempts=10&retry_delay=10'
+    url_params = pika.URLParameters(amqp_url)
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+    connection = pika.BlockingConnection(url_params)
+    channel = connection.channel()
+    channel.queue_declare(queue='vacdrive', durable=True)
     properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=properties)
-
+    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=pika.BasicProperties(delivery_mode=2))
+    print("Produced the message")
+    channel.close()
+    connection.close()
