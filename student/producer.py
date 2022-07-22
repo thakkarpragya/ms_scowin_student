2b02ebc2cb5b03cd8b1b0d96e666b6cd857bff92
Akshaya Shankar <98697587+caput-draconis@users.noreply.github.com>
2022-07-22 15:16:52 +0530

    Update producer.py

--- student/producer.py
+++ student/producer.py
@@ -1,7 +1,8 @@
 import json
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
 
 def publish(method, body):
