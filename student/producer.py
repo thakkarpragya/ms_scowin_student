4364fb8bfe36caa1605e9aaf52192cbb29320edc
Akshaya Shankar <98697587+caput-draconis@users.noreply.github.com>
2022-07-23 15:39:14 +0530

    Update producer.py

--- student/producer.py
+++ student/producer.py
@@ -1,11 +1,19 @@
 import json
+from unittest.mock import DEFAULT
 import pika
-
-# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
-connection = pika.BlockingConnection(pika.ConnectionParameters('flamboyant_neumann:15672'))
-channel = connection.channel()
+# import os
 
 def publish(method, body):
-    properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=properties)
+    amqp_url = 'amqp://rabbit_mq?connection_attempts=10&retry_delay=10'
+    url_params = pika.URLParameters(amqp_url)
+    connection = pika.BlockingConnection(url_params)
+    channel = connection.channel()
+    # amqp_url = os.environ['AMQP_URL']
 
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+    channel.queue_declare(queue='vacdrive')
+    # properties = pika.BasicProperties(content_type=method)
+    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=pika.BasicProperties(content_type=method,delivery_mode=2))
+    print("Produced the message")
+    channel.close()
+    connection.close()
