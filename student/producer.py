c0681d10ca5895efcdb91207a9c1745bc35d1075
caput-draconis <2021cfse014@wilp.bits-pilani.ac.in>
2022-07-23 10:02:18 +0530

    Build Updated Queue communication

--- student/producer.py
+++ student/producer.py
@@ -4,15 +4,16 @@ import pika
 # import os
 
 def publish(method, body):
-    # amqp_url = os.environ['AMQP_URL']
     amqp_url = 'amqp://rabbit_mq?connection_attempts=10&retry_delay=10'
     url_params = pika.URLParameters(amqp_url)
-# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
     connection = pika.BlockingConnection(url_params)
     channel = connection.channel()
-    channel.queue_declare(queue='vacdrive', durable=True)
-    properties = pika.BasicProperties(method)
-    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=pika.BasicProperties(delivery_mode=2))
+    # amqp_url = os.environ['AMQP_URL']
+
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', heartbeat=600, blocked_connection_timeout=300))
+    channel.queue_declare(queue='vacdrive')
+    # properties = pika.BasicProperties(content_type=method)
+    channel.basic_publish(exchange='', routing_key='vacdrive', body=json.dumps(body), properties=pika.BasicProperties(content_type=method,delivery_mode=2))
     print("Produced the message")
     channel.close()
     connection.close()
